cbd973bc180534434c6b60e5cf10fc2b994a45e0
eduarthpaul <eduarthpaul@gmail.com>
2026-03-29 12:18:51 +0300

    Etapa dezvoltare pagina articole si individual articole

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://radioterapeut.ro',
   vite: {
     plugins: [tailwindcss()]
   }
